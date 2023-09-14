69d990b2c1de11be31378190a2cb5a1af1ba7c5f
Celo Reis <m.reis@portaltelemedicina.com.br>
2023-09-14 10:12:52 -0300

    feat: basic sdk config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+
+import { PortalSdk } from "portal-sdk";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+
+const portalSDK = new PortalSdk({
+  containerId: "portal-sdk-container",
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    portalSDK.initialize();
+
+    return () => {
+      portalSDK.destroy();
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ResponsiveAppBar />
+      <div id="portal-sdk-container"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
