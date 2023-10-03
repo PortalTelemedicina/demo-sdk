6ae33d9135e72680cb15269e8ea667612905bf5b
Celo Reis <m.reis@portaltelemedicina.com.br>
2023-10-03 11:44:46 -0300

    feat: demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
+import { Button } from "@mui/material";
 
-import { PortalSdk } from "portal-sdk";
+import { PortalSdk } from "@ptm-screening/portal-sdk";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 const portalSDK = new PortalSdk({
   containerId: "portal-sdk-container",
+  iframeSDKUrl: "https://comforting-bublanina-d11568.netlify.app/",
 });
 
 function App() {
@@ -20,6 +22,25 @@ function App() {
     <div>
       <ResponsiveAppBar />
       <div id="portal-sdk-container"></div>
+
+      <Button
+        variant="outlined"
+        onClick={() =>
+          portalSDK.startVitalMeasurement({
+            age: 30,
+            height: 180,
+            antihypertensive: "1",
+            bloodpressuremedication: "1",
+            diabetes: "type1",
+            gender: "male",
+            identifier: "123456789",
+            smoking: "0",
+            weight: 80,
+          })
+        }
+      >
+        Start
+      </Button>
     </div>
   );
 }
--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -59,7 +59,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            QualyCopos
+            App Demo
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -115,7 +115,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            QualyCopos
+            App Demo
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
