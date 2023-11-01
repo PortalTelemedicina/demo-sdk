f2b3facb45ffe21b5753db4fb93b91092a410cbd
Celo Reis <m.reis@portaltelemedicina.com.br>
2023-11-01 08:59:54 -0300

    feat: new demo with appointment

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 
 import { PortalSdk } from "@ptm-screening/portal-sdk";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import { getToken } from "./getToken";
 
-let portalSDK: PortalSdk;
+type TokeStateType = {
+  value: string;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+let portalSDK: PortalSdk | undefined;
 
 function App() {
-  useEffect(() => {
-    getToken()
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .then((response: any) => {
-        portalSDK = new PortalSdk({
-          containerId: "portal-sdk-container",
-          iframeSDKUrl: import.meta.env.DEV
-            ? "http://localhost:3003"
-            : "https://dev.app.screening.portaltelemedicina.com.br/",
-          token: response.token,
-        });
+  const [token, setToken] = useState<TokeStateType>({
+    value: "",
+    isLoading: false,
+    isError: false,
+  });
 
-        portalSDK.initialize();
+  useEffect(() => {
+    if (token.isLoading || token.value) {
+      return;
+    }
+
+    setToken({ value: "", isLoading: true, isError: false });
+
+    getToken()
+      .then((response) => {
+        setToken({ value: response.token, isLoading: false, isError: false });
       })
-      .catch((err) => console.error(err));
+      .catch(() => {
+        setToken({ value: "", isLoading: false, isError: true });
+      });
+  }, [token]);
+
+  useEffect(() => {
+    if (token.value) {
+      portalSDK = new PortalSdk({
+        containerId: "portal-sdk-container",
+        iframeSDKUrl: import.meta.env.DEV
+          ? "http://localhost:3003"
+          : "https://dev.app.screening.portaltelemedicina.com.br/",
+        token: token.value,
+        appointmentURL: "localhost:4200",
+      });
+
+      portalSDK.initialize();
+    }
 
     return () => {
       portalSDK?.destroy();
     };
-  }, []);
+  }, [token.value]);
+
+  if (token.isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
@@ -37,7 +67,7 @@ function App() {
       <Button
         variant="outlined"
         onClick={() =>
-          portalSDK.startVitalMeasurement({
+          portalSDK?.startVitalMeasurement({
             age: 30,
             height: 180,
             antihypertensive: "1",
@@ -50,7 +80,40 @@ function App() {
           })
         }
       >
-        Start
+        Iniciar medidas vitais
+      </Button>
+
+      <Button
+        variant="outlined"
+        onClick={() =>
+          portalSDK?.startPatientAppointment(
+            "a.garbiati+qc+p1@portaltelemedicina.com.br"
+          )
+        }
+      >
+        Iniciar consulta paciente
+      </Button>
+
+      <Button
+        variant="outlined"
+        onClick={() =>
+          portalSDK?.startDoctorAppointment(
+            "a.garbiati+qc+spec2@portaltelemedicina.com.br"
+          )
+        }
+      >
+        Iniciar consulta médico
+      </Button>
+
+      <Button
+        variant="outlined"
+        onClick={() =>
+          portalSDK?.startOperatorAppointment(
+            "a.garbiati+qc+op1@portaltelemedicina.com.br"
+          )
+        }
+      >
+        Iniciar consulta operador
       </Button>
     </div>
   );
